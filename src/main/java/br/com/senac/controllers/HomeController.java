110d693a9c62c6e5dcea4c7e74b2b9005c47c6c9
Janaina-frv <janainafrv@gmail.com>
2021-11-28 09:56:41 -0300

    Ajustes nas telas para navegação

--- src/main/java/br/com/senac/controllers/HomeController.java
+++ src/main/java/br/com/senac/controllers/HomeController.java
@@ -14,6 +14,18 @@ public class HomeController {
 		ModelAndView mv = new ModelAndView("index");
 		return mv;
 	}
+	
+	@GetMapping("login")
+	public ModelAndView Login() {
+		ModelAndView mv = new ModelAndView("login");
+		return mv;
+	}
+	
+	@GetMapping("login2")
+	public ModelAndView Login2() {
+		ModelAndView mv = new ModelAndView("login2");
+		return mv;
+	}
 
 
 }
