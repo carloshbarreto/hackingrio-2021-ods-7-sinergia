e9246d8c04cf7a05184879b86aca7e24035f8265
Carlos Henrique Barreto da Conceicao <carlos_henrique47@yahoo.com.br>
2021-11-27 22:19:09 -0300

    Criação das telas de Informação e Meu Consumo

--- src/main/java/br/com/senac/controllers/InformacoesController.java
+++ src/main/java/br/com/senac/controllers/InformacoesController.java
@@ -1,23 +1,17 @@
 package br.com.senac.controllers;
 
-
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-
-
-
 @Controller
-@RequestMapping("listagem")
-public class ListagemController {
-	
-	
+@RequestMapping("informacoes")
+public class InformacoesController {
+
 	@GetMapping("")
 	public ModelAndView Listar() {
-		ModelAndView mv = new ModelAndView("meuConsumo");
+		ModelAndView mv = new ModelAndView("informacoes");
 		return mv;
 	}
 
--- src/main/java/br/com/senac/controllers/MeuConsumoController.java
+++ src/main/java/br/com/senac/controllers/MeuConsumoController.java
@@ -1,23 +1,17 @@
 package br.com.senac.controllers;
 
-
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-
-
-
 @Controller
-@RequestMapping("listagem")
-public class ListagemController {
-	
+@RequestMapping("meuconsumo")
+public class MeuConsumoController {
 	
 	@GetMapping("")
 	public ModelAndView Listar() {
-		ModelAndView mv = new ModelAndView("meuConsumo");
+		ModelAndView mv = new ModelAndView("cadastro-consumo");
 		return mv;
 	}
 
